5e89669c2463f8d84236025c5417e2e8d569d719
vibortolotti <vibortolotti@4871b473-ea4a-0410-af9d-417399f00a90>
2008-04-15 16:44:02 +0000

    <classe "AnalisadorLexico" incluida e integrada com: "AutomatoFD" e "TabelaSimbolos">

---
+++ includes/AnalisadorLexico.h
@@ -0,0 +1,37 @@
+#ifndef ANALISADORLEXICO_H_
+#define ANALISADORLEXICO_H_
+
+#include <iostream>
+#include <list>
+#include <map>
+#include <string>
+
+#include "./AutomatoFD.h"
+#include "./StructToken.h"
+
+
+
+class AnalisadorLexico
+{
+	public:
+		AnalisadorLexico( const std::string );
+
+		~AnalisadorLexico( );
+
+	protected:
+
+	private:
+		AutomatoFD*
+		automato;
+
+		std::list<std::string>
+		codigoFonte;
+
+		std::map<int, StructToken>
+		saidaAnalisadorLexico;
+
+		void
+		carregaCodigo( const std::string );
+};
+
+#endif /*ANALISADORLEXICO_H_*/
